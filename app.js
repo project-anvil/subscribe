696e1d74df39e2079f7d2ba80564a7c08de22430
Arcaelyx <arcaelyx@protonmail.com>
2019-01-31 17:39:48 -0500

    Leverage subscriber mutation on POST requests
    In order to accept incoming POST requests to save subscribers, the
    previously defined `saveSubscriber` boundary is not integrated to
    express to receive JSON payloads for inserting subscribers to the
    databse.

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const { saveSubscriber } = require('./src/boundaries')
+
 const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.post('/', (req, res, next) =>
+  saveSubscriber(req.body)
+    .then(() => res.status(201).send())
+    .catch(next)
+)
 
 module.exports = app;
