22cc26b916cad8aa05d4a47cb90e231745924bae
Arcaelyx <arcaelyx@protonmail.com>
2019-01-31 17:31:29 -0500

    Define database entity for subscriber additions
    In order to store subscribers into the database in a way that's
    decoupled from knex, a subscriber entity is defined that makes use of a
    simple read/write interface to be used across the application codebase.

--- src/entities/index.js
+++ src/entities/index.js
@@ -2,3 +2,9 @@ const config = require('../../config.json')
 const entityMap = require('../utils/entityMap')
 
 const entityMapper = entityMap(config)
+
+const subscriber = entityMapper.define('subscriber', ['id', 'email'])
+
+module.exports = {
+  subscriber
+}
